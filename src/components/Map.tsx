5dc54b42da9a3d30d17bfcc998fac6d2062f2d42
yassine ben azouz <yassinebenazouz123@gmail.com>
2023-05-14 16:18:59 +0200

    made the map change when ip changes

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,26 +1,32 @@
 import React, { useContext } from 'react'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 import { ipDataContext } from '../context/ipDataContext'
 function Map() {
     const { ipLocation } = useContext(ipDataContext)
 
     return (
         <div>
-            <MapContainer center={ipLocation} zoom={13} scrollWheelZoom={false}>
+            {ipLocation && <MapContainer center={ipLocation} zoom={13} scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={ipLocation!}>
+                <Locater />
+                {/* <Marker position={ipLocation!}>
                     <Popup>
                         A pretty CSS3 popup. <br /> Easily customizable.
                     </Popup>
-                </Marker>
-            </MapContainer>
-
-
+                </Marker> */}
+            </MapContainer>}
         </div>
     )
+}
+function Locater() {
+    const { ipLocation } = useContext(ipDataContext)
+
+    const map = useMap()
+    map.flyTo(ipLocation!)
+    return null
 }
 
 export default Map
